bac96111a08716659ce57ee4846d4915eb31344f
Latifaaaah <115138970+Latifahtnadiyah@users.noreply.github.com>
2023-10-04 13:52:06 +0700

    Add files via upload

---
+++ sesi2/tugasjs#2.js
@@ -0,0 +1,8 @@
+let firstName = "Latifah";
+let lastName = "Tasliyatunadiyah";
+let age = 22;
+let isMarried = false;
+
+console.log(`Nama Lengkap : ${firstName} ${lastName}`);
+console.log(`Umur : ${age}`);
+console.log(`Is Married : ${isMarried}`);
